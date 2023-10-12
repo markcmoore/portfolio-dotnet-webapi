97a94c923ee5eb4cccca0ad9f46af6a3d90b8c31
Mark Moore <mark.c.moore@hotmail.com>
2023-10-12 15:24:42 -0500

    added a test

--- portfolio_website_testing/Controller_Testing/Controllers_testing.cs
+++ portfolio_website_testing/Controller_Testing/Controllers_testing.cs
@@ -10,9 +10,8 @@ using Microsoft.Extensions.Logging.Abstractions;
 using portfolio_business_logic;
 using portfolio_website.Controllers;
 using portfolio_website_models;
-using portfolio_website_repo;
-using Xunit;
-using Microsoft.Extensions.Logging;
+// using portfolio_website_repo;
+// using Microsoft.Extensions.Logging;
 
 namespace portfolio_website_testing
 {
@@ -21,6 +20,16 @@ namespace portfolio_website_testing
     /// </summary>
     public class Controllers_testing
     {
+
+        #region arrays to send to the different action methods.
+        public static readonly IEnumerable<object[]> _RegisterNewAccountTestArray = new List<object[]>(){
+            new object[] {new RegisterModel(){Username = "success"} },
+            new object[] {new RegisterModel(){Username = "username"} },
+            new object[] {new RegisterModel(){Username = "password"} },
+            new object[] {new RegisterModel(){Username = "both"} }
+        };
+
+        #endregion
         // private readonly IConfiguration _configuration;
         // private readonly IRegister_Repo_Access _repo;
         private readonly ILogger<RegisterController> _logger;
@@ -48,37 +57,48 @@ namespace portfolio_website_testing
             Assert.True(myDouble == (myInt * 2));
         }
 
-        [Fact]
-        public async void RegisterNewAccountAsync_ReturnsRegisteredAccount()
+        [Theory]
+        [MemberData(nameof(_RegisterNewAccountTestArray))]
+        public async void RegisterNewAccountAsync_ReturnsRegisteredAccount(RegisterModel rm)
         {
-            // ARRANGE
-            RegisteredAccount mockAccount = new RegisteredAccount()
-            {
-                AccountId = 1,
-                CreatedOn = DateTime.Now,
-                FirstName = "",
-                LastName = "",
-                Occupation = "test",
-            };
-            RegisterModel mockRegisterModel = new RegisterModel();
-
+            // ARRANGE - done above
             // ACT
             // get the actionresult containing the mocked return
-            ActionResult<IDictionary<string, RegisteredAccount>> retDictionary = await this._registerController.RegisterNewAccountAsync(mockRegisterModel);
-
-            // Console.WriteLine(retDictionary.Result);
-            // Console.WriteLine(retDictionary.Value);
-
-            // extract the IDictionary from the ActionResult
-            if (retDictionary.Result is CreatedResult)
-            {
-                IDictionary<string, RegisteredAccount>? ret = (retDictionary.Result as CreatedResult)?.Value as IDictionary<string, RegisteredAccount>;
-            }
+            ActionResult<string> retDictionary = await this._registerController.RegisterNewAccountAsync(rm);
 
             // ASSERT
-            Assert.True(2 == 2);
+            if (retDictionary.Result is CreatedResult)
+            {
+                Assert.True(rm.Username == "success");
+            }
+            else if (retDictionary.Result is BadRequestObjectResult)
+            {
+                // test for the three possible returns based on the username sent in.
+                if (rm.Username == "username")
+                {
+                    Assert.True(((retDictionary.Result as BadRequestObjectResult)?.Value as string) == "There is already an account with that username");
+                }
+                else if (rm.Username == "password")
+                {
+                    Assert.True(((retDictionary.Result as BadRequestObjectResult)?.Value as string) == "There is already an account with that password");
+                }
+                else if (rm.Username == "both")
+                {
+                    Assert.True(((retDictionary.Result as BadRequestObjectResult)?.Value as string) == "That username and password already exist.");
+                }
+                else Assert.Fail("A test case in RegisterNewAccountAsync_ReturnsRegisteredAccount failed");
+            }
+            else if (retDictionary.Result is BadRequestResult)
+            {
+                Assert.Fail("Something unexpected went wrong in RegisterNewAccountAsync_ReturnsRegisteredAccount");
+            }
+            else Assert.Fail("A test case in RegisterNewAccountAsync_ReturnsRegisteredAccount failed");
         }
 
+        /// <summary>
+        /// Complete - 
+        /// </summary>
+        /// <returns></returns>
         [Fact]
         public async void AccountInfo_ReturnsRegisteredAccount()
         {
