ad43c91160ff83cac25d0422d891cbc4fcf587ac
Mark Moore <mark.c.moore@hotmail.com>
2023-10-17 13:37:43 -0500

    test repo layer access to testingDb in Azure fron Github

--- portfolio_website_testing/Repository_Testing/Repository_testing.cs
+++ portfolio_website_testing/Repository_Testing/Repository_testing.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.RenderTree;
+using portfolio_website_testing.DatabaseReset;
 using Xunit;
 
 namespace portfolio_website_testing
@@ -9,9 +11,16 @@ namespace portfolio_website_testing
     public class Repository_testing
     {
         [Fact]
-        public void Test1()
+        public async void CreateTablesAsync_SetsUpTableProofOfConcept()
         {
-            Assert.True(true);
+            // ARRANGE
+            DatabaseSetup dbSetup = new DatabaseSetup();
+
+            // ACT
+            await dbSetup.CreateTablesAsync();
+
+            //ASSERT
+            // Assert.True(true);
         }
     }
 }
