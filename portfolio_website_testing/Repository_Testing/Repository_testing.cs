dc7349c0a0d4c0c8050544b2bfca8b520bc26887
Mark Moore <mark.c.moore@hotmail.com>
2023-10-18 08:20:53 -0500

    adds testingDB connection string directly to testing proj

--- portfolio_website_testing/Repository_Testing/Repository_testing.cs
+++ portfolio_website_testing/Repository_Testing/Repository_testing.cs
@@ -19,7 +19,8 @@ namespace portfolio_website_testing
                 .AddUserSecrets<Repository_testing>()
                 .AddEnvironmentVariables()
                 .Build();
-            this._TestingDbConStr = this._configuration.GetConnectionString("TestingDb");// TODO:haven't been able to get this to work with github actions
+            // this._TestingDbConStr = this._configuration.GetConnectionString("TestingDb");// TODO:haven't been able to get this to work with github actions
+            this._TestingDbConStr = "Server=tcp:portfolio-website-server.database.windows.net,1433;Initial Catalog=portfolio-website-testing-db;Persist Security Info=False;User ID=portfolio-db;Password=marks1websiteDb;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             Console.WriteLine(this._TestingDbConStr);
         }
 
